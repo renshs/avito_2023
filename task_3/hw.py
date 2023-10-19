74ab02709899ed0393ff24908f54a17e497ddcb4
Garaev Kamil <garaeff.camil@yandex.ru>
2023-10-19 11:43:36 +0300

    typing for vocab added

--- task_3/hw.py
+++ task_3/hw.py
@@ -1,10 +1,10 @@
-from typing import List
+from typing import List, Dict
 from collections.abc import Iterable
 
 
 class CountVectorizer:
     def __init__(self) -> None:
-        self.vocabulary = {}
+        self.vocabulary: Dict[str: int] = {}
 
     def fit_transform(self, data: list) -> List[list]:
         """
